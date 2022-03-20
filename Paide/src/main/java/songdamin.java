18e086d6d8ea49d9ab78ac888a1bc2a99bc4a0ee
smhrd <smhrd@211.223.37.232>
2022-03-21 08:59:01 +0900

    MVC모델

--- Paide/src/main/java/songdamin.java
+++ Paide/src/main/java/songdamin.java
@@ -1,4 +1,4 @@
-
+																																																																																																																																																																																																																																																																																																																																																																																																										
 public class songdamin {
 	public static void main(String[] args) {
 		
